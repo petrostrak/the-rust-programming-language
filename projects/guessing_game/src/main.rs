4b9ba769974f3c0a928af5416602b11cd1f2a0ed
petrostrak <pit.trak@gmail.com>
2022-04-06 12:59:11 +0300

    Generate a random number

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -1,9 +1,12 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
 
     println!("Please input your guess.");
+
+    let secret_number = rand::thread_rng().gen_range(1,101);
 
     // guess is mutable therefore it can be assigned
     // to different values.
@@ -12,5 +15,6 @@ fn main() {
     io::stdin().read_line(&mut guess)
         .expect("Failed to read line");
 
-    println!("You guessed: {}", guess)
+    println!("The secret number is {}", secret_number);
+    println!("You guessed: {}", guess);
 }
