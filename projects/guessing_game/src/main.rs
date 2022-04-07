5c20a720255d03dea69b2ee1b59733d49fec7d9f
petrostrak <pit.trak@gmail.com>
2022-04-07 09:59:22 +0300

    Loop

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -6,28 +6,32 @@ fn main() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1,101);
-    println!("The secret number is {}", secret_number);
 
-    println!("Please input your guess.");
+    loop { 
+        println!("Please input your guess.");
 
-    // guess is mutable therefore it can be assigned
-    // to different values.
-    let mut guess = String::new();
+        // guess is mutable therefore it can be assigned
+        // to different values.
+        let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess)
+        io::stdin().read_line(&mut guess)
         .expect("Failed to read line");
 
-    // We shadow the previous value of guess with a new one.
-    // This feature is often used in situations in which you
-    // want to convert a value from one type to another type.
-    let guess: u32 = guess.trim().parse()
-        .expect("Please type a number!");
+        // We shadow the previous value of guess with a new one.
+        // This feature is often used in situations in which you
+        // want to convert a value from one type to another type.
+        let guess: u32 = guess.trim().parse()
+            .expect("Please type a number!");
 
-    println!("You guessed: {}", guess);
+        println!("You guessed: {}", guess);
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!")
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
