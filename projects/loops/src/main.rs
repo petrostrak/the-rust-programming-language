25b2a45052554ed5961d55e7c295ea88e5eace44
petrostrak <pit.trak@gmail.com>
2022-04-10 15:19:08 +0300

    loops

---
+++ projects/loops/src/main.rs
@@ -0,0 +1,36 @@
+fn main() {
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {}", result);
+
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{}!", number);
+
+        number -= 1;
+    }
+
+    println!("LIFTOFF!!!");
+
+    let a:[i32; 5] = [10, 20, 30, 40, 50];
+    let mut index = 0;
+
+    while index < 5 {
+        println!("the values is {}", a[index]);
+        index += 1;
+    }
+
+    for element in a.iter() {
+        println!("{}", element+1);
+    }
+
+}
