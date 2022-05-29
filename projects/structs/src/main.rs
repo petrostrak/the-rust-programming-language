34ba9e9929148685701f50eab19b656d6e168fe2
petrostrak <pit.trak@gmail.com>
2022-05-29 20:53:13 +0300

    structs

--- projects/structs/src/main.rs
+++ projects/structs/src/main.rs
@@ -3,8 +3,8 @@ fn main() {
     let Petros = User{
         email: String::from("someone@example.com"),
         username: String::from("PetrosTrak"),
-        active: true,
-        sign_in_count: 1,
+        active: false,
+        sign_in_count: 0,
     };
 
     fn build_user(username: String, email: String, counter: u64, active: bool) -> User {
@@ -16,8 +16,14 @@ fn main() {
         }
     }
 
+    let Alex = User {
+        username: String::from("alx"),
+        email: String::from("alx@example.com"),
+        ..Petros
+    };
+
     let Maggie = build_user("Maggie".to_string(), "mag@example.com".to_string(), 20, false);
-    
+
 }
 
 struct User {
