4383314724675ae13333bbffd03706ed8af97089
petrostrak <pit.trak@gmail.com>
2022-05-29 20:31:27 +0300

    slices

--- projects/slices/src/main.rs
+++ projects/slices/src/main.rs
@@ -1,16 +1,24 @@
+// Slices let you reference a contiguous sequence of elements in a collection rather
+// than the whole collection.
 fn main() {
     let s = String::from("Hello world!");
-    first_word(&s);
+    let word = first_word(&s[..]);
+    // s.clear();
+    println!("{}", word)
+
+    // let hello = &s[..5];
+    // let world = &s[6..];
+    // println!("{} {}", hello, world);
 }
 
-fn first_word(s: &String) -> usize {
+fn first_word(s: &str) -> &str {
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
         if item ==  b' ' {
-            return i;
+            return &s[..i];
         }
     }
 
-    s.len()
+    &s[..]
 }
