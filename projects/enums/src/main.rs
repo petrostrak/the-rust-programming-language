01ea29d97cac40af3a17ee8227791408c44bf0a5
petrostrak <pit.trak@gmail.com>
2022-05-30 11:01:16 +0300

    enums

---
+++ projects/enums/src/main.rs
@@ -0,0 +1,21 @@
+enum IpAddrKind {
+    V4,
+    V6,
+}
+
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
+
+fn main() {
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+
+    let localhost = IpAddr{
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1")
+    };
+}
+
+fn route(ip_kind: IpAddrKind) {} 
