1f3d9e7e3f35d3c08bfb24ed79f73d80d3c6e4b1
petrostrak <pit.trak@gmail.com>
2022-05-30 11:08:42 +0300

    enums

--- projects/enums/src/main.rs
+++ projects/enums/src/main.rs
@@ -3,6 +3,19 @@ enum IpAddrKind {
     V6,
 }
 
+enum Message {
+    Quit,
+    Move {x:i32, y:i32},
+    Write(String),
+    ChangeColor(i32,i32, i32),
+}
+
+impl Message {
+    fn some_func() {
+        println!("Let's get Rusty!")
+    }
+}
+
 struct IpAddr {
     kind: IpAddrKind,
     address: String,
@@ -16,6 +29,8 @@ fn main() {
         kind: IpAddrKind::V4(127, 0, 0, 1),
         address: String::from("127.0.0.1")
     };
+
+    Message::some_func()
 }
 
 fn route(ip_kind: IpAddrKind) {} 
