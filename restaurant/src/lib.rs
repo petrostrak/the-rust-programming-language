9a4499552efd4e2db2d31b75d2b51f9cd8894c9c
petrostrak <pit.trak@gmail.com>
2022-06-04 19:17:45 +0300

    Modules

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,21 +1,7 @@
 use rand::{Rng, CryptoRng, ErrorKind::Transient};
 use std::io::*;
 
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
-
-        fn seat_at_table() {}
-    }
-
-    mod serving {
-        fn take_order() {}
-
-        fn serve_order() {}
-
-        fn take_payment() {}
-    }
-}
+mod front_of_house;
 
 fn serve_order() {}
 
