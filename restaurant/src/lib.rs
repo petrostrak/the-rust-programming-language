ba161f6a238f2e3471f2bf695ee1645e58b3aeac
petrostrak <pit.trak@gmail.com>
2022-06-04 19:04:04 +0300

    Modules

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -14,14 +14,6 @@ mod front_of_house {
     }
 }
 
-pub fn eat_at_restaurant() {
-    // Absolut path
-    crate::front_of_house::hosting::add_to_waitlist();
-
-    // Relative path
-    front_of_house::hosting::add_to_waitlist();
-}
-
 fn serve_order() {}
 
 mod back_of_house {
@@ -31,4 +23,19 @@ mod back_of_house {
     }
 
     fn cook_order() {}
+
+    pub struct Breakfast {
+        toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast { toast: String::from(toast), seasonal_fruit: String::from("peaches") }
+        }
+    }
+}
+
+pub fn eat_at_restaurant() {
+    let mut meal = back_of_house::Breakfast::summer("Rey");
 }
