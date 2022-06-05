a7a190bbeb993d0b7e64714109523b50b79b2d6d
petrostrak <pit.trak@gmail.com>
2022-06-05 18:37:08 +0300

    vectors

--- projects/collections/src/main.rs
+++ projects/collections/src/main.rs
@@ -27,6 +27,24 @@ fn main() {
    }
 
    for i in &v3 {
-    println!("{}", i)
+    println!("{}", i);
+   }
+
+   enum SpreadsheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String),
+    }   
+
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12),
+    ];
+
+    match &row[1] {
+        SpreadsheetCell::Int(i) => println!("{}", i),
+        _ => println!("Not an integer")
+    }
 }
-}
+
