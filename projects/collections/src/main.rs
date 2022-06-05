72868f003caf255078f44e16ad18fbad09fef70a
petrostrak <pit.trak@gmail.com>
2022-06-05 18:25:33 +0300

    vectors

--- projects/collections/src/main.rs
+++ projects/collections/src/main.rs
@@ -7,8 +7,9 @@ fn main() {
 
    { let v2 = vec![1, 2, 3];}
 
-   let third = &v[2];
-//    println!("The third element is {}", third);
+   let third = &v[2]; // immutable reference to value of vector
+//    v.push(6); // mutable reference to push to vector. We expect the underlying value not to change
+   println!("The third element is {}", third); // we use the immutable reference.
 
    match v.get(2) {
        Some(third) => println!("The third element is {}", third),
