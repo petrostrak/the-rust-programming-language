0e09f1f7d90534eff85e8768942642b28d4e3c26
petrostrak <pit.trak@gmail.com>
2022-06-06 16:57:40 +0300

    generics

---
+++ projects/generics/src/main.rs
@@ -0,0 +1,33 @@
+fn main() {
+    let number_list = vec![34, 50, 100, 65];
+
+    let largest = get_largest(number_list);
+
+    println!("The largest number is {}", largest);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let largest = get_largest(char_list);
+
+    println!("The largest number is {}", largest);
+}
+
+fn get_largest<T: PartialOrd + Copy>(number_list: Vec<T>) -> T {
+    let mut largest = number_list[0];
+    for number in number_list {
+        if number > largest {
+            largest = number;
+        }
+    }
+    largest
+}
+
+fn get_largest_char(number_list: Vec<char>) -> char {
+    let mut largest = number_list[0];
+    for number in number_list {
+        if number > largest {
+            largest = number;
+        }
+    }
+    largest
+}
