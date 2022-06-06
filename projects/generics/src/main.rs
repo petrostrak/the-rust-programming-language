53a781602a13ab585854cb947623e5f22777208c
petrostrak <pit.trak@gmail.com>
2022-06-06 17:02:01 +0300

    generics in structs

--- projects/generics/src/main.rs
+++ projects/generics/src/main.rs
@@ -10,6 +10,15 @@ fn main() {
     let largest = get_largest(char_list);
 
     println!("The largest number is {}", largest);
+
+    let p1 = Point{x:5, y:10};
+    let p2 = Point{x: 1.5, y: 3.3};
+    let p3 = Point{x: 1.5, y: 3};
+}
+
+struct Point<T, U> {
+    x: T,
+    y: U,
 }
 
 fn get_largest<T: PartialOrd + Copy>(number_list: Vec<T>) -> T {
