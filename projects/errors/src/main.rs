469c4938c26fc578d4fbff36c63ada7f307308f4
petrostrak <pit.trak@gmail.com>
2022-06-06 12:19:46 +0300

    errors

--- projects/errors/src/main.rs
+++ projects/errors/src/main.rs
@@ -4,20 +4,21 @@ use std::io::ErrorKind;
 fn main() {
     // a();
 
-    let f = File::open("hello.txt");
+    let f = File::open("hello.txt").expect("failed to open hello.txt");
+    // let f = File::open("hello.txt").unwrap();
 
-    let f = match  f {
-        Ok(file) => file,
-        Err(error) => match error.kind() {
-            ErrorKind::NotFound => match File::create("hello.txt") {
-                Ok(fc) => fc,
-                Err(e) => panic!("problem creating the file: {:?}", e),
-            }
-            other_error => {
-                panic!("problem opening the file: {:?}", other_error)
-            }
-        }
-    };
+    // let f = match  f {
+    //     Ok(file) => file,
+    //     Err(error) => match error.kind() {
+    //         ErrorKind::NotFound => match File::create("hello.txt") {
+    //             Ok(fc) => fc,
+    //             Err(e) => panic!("problem creating the file: {:?}", e),
+    //         }
+    //         other_error => {
+    //             panic!("problem opening the file: {:?}", other_error)
+    //         }
+    //     }
+    // };
 }
 
 fn a() {
