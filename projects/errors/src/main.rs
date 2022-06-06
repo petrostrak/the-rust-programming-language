b739b9a6e573455f6198babafb19b6181e9bc3d2
petrostrak <pit.trak@gmail.com>
2022-06-06 16:36:25 +0300

    Error propagation

--- projects/errors/src/main.rs
+++ projects/errors/src/main.rs
@@ -1,10 +1,26 @@
 use std::fs::File;
+use std::io::{self, Read};
 use std::io::ErrorKind;
 
 fn main() {
     // a();
 
-    let f = File::open("hello.txt").expect("failed to open hello.txt");
+    fn read_username_from_file() -> Result<String, io::Error> {
+        let f = File::open("hello.txt");
+
+        let mut f = match f {
+            Ok(file) => file,
+            Err(e) => return  Err(e)
+        };
+
+        let mut s = String::new();
+
+        match f.read_to_string(&mut s) {
+            Ok(_) => Ok(s),
+            Err(e) => Err(e),
+        }
+    }
+    
     // let f = File::open("hello.txt").unwrap();
 
     // let f = match  f {
