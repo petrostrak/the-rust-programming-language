7aac6d304986d5f066ea0d4528af63b1b3281dbd
petrostrak <pit.trak@gmail.com>
2022-06-10 13:42:25 +0300

    Implement the Deref trait

--- the-book/deref_and_drop/src/main.rs
+++ the-book/deref_and_drop/src/main.rs
@@ -1,3 +1,5 @@
+use std::ops::Deref;
+
 struct MyBox<T>(T);
 
 impl<T> MyBox<T> {
@@ -6,10 +8,18 @@ impl<T> MyBox<T> {
     }
 }
 
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        &self.0
+    }
+}
+
 fn main() {
     let x = 5;
     let y = MyBox::new(x);
 
     assert_eq!(5, x);
-    assert_eq!(5, *y);
+    assert_eq!(5, *(y.deref()));
 }
