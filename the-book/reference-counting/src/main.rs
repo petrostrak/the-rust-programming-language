7083984af4eef3a71dc20e4a3fa4430f59065f64
petrostrak <pit.trak@gmail.com>
2022-06-13 20:33:12 +0300

    Increasing the reference count

--- the-book/reference-counting/src/main.rs
+++ the-book/reference-counting/src/main.rs
@@ -9,6 +9,12 @@ use crate::List::{Cons, Nil};
 
 fn main() {
     let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("count after creating a = {}", Rc::strong_count(&a));
     let b = Cons(3, Rc::clone(&a));
-    let c = Cons(3, Rc::clone(&a));
+    println!("count after creating b = {}", Rc::strong_count(&a));
+    {
+        let c = Cons(3, Rc::clone(&a));
+        println!("count after creating c = {}", Rc::strong_count(&a));
+    }
+    println!("count after creating c goes out of scope = {}", Rc::strong_count(&a));
 }
