393a9ef0562ad39655d18ab331244220a6964fbf
petrostrak <pit.trak@gmail.com>
2022-06-14 16:25:55 +0300

    Implementing the Trait

--- the-book/trait_objects/src/main.rs
+++ the-book/trait_objects/src/main.rs
@@ -1,3 +1,34 @@
+use trait_objects::{Screen, Button, Draw};
+
+struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        // draw select box
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let screen = Screen {
+        components: vec![
+            Box::new(SelectBox {
+                width: 100,
+                height: 100,
+                options: vec![
+                    String::from("yes"),
+                    String::from("no"),
+                    String::from("maybe"),
+                ]
+            }),
+            Box::new(Button {
+                width: 100,
+                height: 100,
+                label: String::from("ok")
+            })
+        ],
+    };
 }
