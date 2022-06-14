3eb1a17c4e13ab5867ff2fd9f758999593751d71
petrostrak <pit.trak@gmail.com>
2022-06-14 20:51:49 +0300

    Object oriented design pattern - define Post

---
+++ the-book/state_design_pattern/src/main.rs
@@ -0,0 +1,15 @@
+use state_design_pattern::Post;
+
+fn main() {
+    let mut post = Post::new();
+
+    post.add_text("I ate a salad for launch today.");
+    assert_eq!("", post.content());
+
+    post.request_review();
+    assert_eq!("", post.content());
+
+    post.approve();
+    assert_eq!("I ate a salad for launch today.", post.content());
+
+}
