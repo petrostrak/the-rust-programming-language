8257a656b55bd7792c8986e37bba4f76a26707fa
petrostrak <pit.trak@gmail.com>
2022-06-14 21:20:13 +0300

    Conditional if let expressions

--- the-book/patterns_and_matching/src/main.rs
+++ the-book/patterns_and_matching/src/main.rs
@@ -19,7 +19,23 @@ fn main() {
         lang => println!("Unsupported language! {:?}", lang),
     }
 
-    // =======================
-    //    Match Expressions
-    // =======================
+    // ===================================
+    //   Conditional if let Expressions
+    // ===================================
+    let authorization_status: Option<&str> = None;
+    let is_admin = false;
+    let group_id: Result<u8, _>  = "34".parse();
+
+    if let Some(status) = authorization_status {
+        println!("Authorization status: {}", status);
+    } else if is_admin {
+        println!("Authorization status: admin");
+    } else if let Ok(group_id) = group_id {
+        if group_id > 30 {
+            println!("Authorization status: privileged")
+        } else {
+            println!("Authorization status: basic")
+        }
+    }
+    
 }
