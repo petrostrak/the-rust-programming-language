19548fd870102579096cc2cee59b762dedfd61aa
petrostrak <pit.trak@gmail.com>
2022-06-15 15:22:48 +0300

    While let Conditional Loops

--- the-book/patterns_and_matching/src/main.rs
+++ the-book/patterns_and_matching/src/main.rs
@@ -37,5 +37,18 @@ fn main() {
             println!("Authorization status: basic")
         }
     }
+
+    // ===================================
+    //   While let Conditional Loops
+    // ===================================
+    let mut stack = Vec::new();
+
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+
+    while let Some(top) = stack.pop() {
+        println!("{}", top)
+    }
     
 }
