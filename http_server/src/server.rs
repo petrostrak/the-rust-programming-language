87b218918a1eee35871554b1407e99cd8adde973
petrostrak <pit.trak@gmail.com>
2023-03-13 10:52:10 +0200

    Utilize TcpListener

--- http_server/src/server.rs
+++ http_server/src/server.rs
@@ -1,17 +1,18 @@
-use std::net::{IpAddr, Ipv4Addr};
+use std::net::TcpListener;
 
 pub struct Server {
-    pub addr: IpAddr,
+    pub addr: String,
     pub port: u32,
 }
 
 impl Server {
     pub fn new(port: u32) -> Self {
-        let addr = IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1));
+        let addr = "127.0.0.1".to_string();
         Self { addr, port }
     }
 
     pub fn run(&self) {
+        let listener = TcpListener::bind(format!("{}:{}", self.addr, self.port));
         println!("Listening on {:?}:{}", self.addr, self.port)
     }
 }
