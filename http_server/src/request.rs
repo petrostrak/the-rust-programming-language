b6d75ad2b1ed127c19df271e8ee5bb2dbc2bf2e7
petrostrak <pit.trak@gmail.com>
2023-03-13 13:20:00 +0200

    Implement custom error type

--- http_server/src/request.rs
+++ http_server/src/request.rs
@@ -1,5 +1,7 @@
 use http::Method;
 use std::convert::TryFrom;
+use std::error::Error;
+use std::fmt::Display;
 
 pub struct Request {
     pub path: String,
@@ -8,8 +10,37 @@ pub struct Request {
 }
 
 impl TryFrom<&[u8]> for Request {
-    type Error = String;
+    type Error = RequestError;
+
+    // GET /search?name=pit&sort=1 HTTP/1.1
     fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
         unimplemented!()
     }
 }
+
+#[derive(Debug)]
+pub enum RequestError {
+    InvalidRequest,
+    InvalidEncoding,
+    InvalidProtocol,
+    InvalidMethod,
+}
+
+impl RequestError {
+    fn message(&self) -> &str {
+        match self {
+            RequestError::InvalidRequest => "Invalid request",
+            RequestError::InvalidEncoding => "Invalid encoding",
+            RequestError::InvalidProtocol => "Invalid protocol",
+            RequestError::InvalidMethod => "Invalid method",
+        }
+    }
+}
+
+impl Display for RequestError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl Error for RequestError {}
