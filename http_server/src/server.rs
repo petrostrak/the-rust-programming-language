4fbe865418292c4aac601a749702707b803f709a
petrostrak <pit.trak@gmail.com>
2023-03-13 11:48:07 +0200

    Read stream from listener

--- http_server/src/server.rs
+++ http_server/src/server.rs
@@ -1,4 +1,4 @@
-use std::net::TcpListener;
+use std::{io::Read, net::TcpListener};
 
 pub struct Server {
     pub addr: String,
@@ -12,7 +12,22 @@ impl Server {
     }
 
     pub fn run(&self) {
-        let listener = TcpListener::bind(format!("{}:{}", self.addr, self.port));
-        println!("Listening on {:?}:{}", self.addr, self.port)
+        let listener = TcpListener::bind(format!("{}:{}", self.addr, self.port)).unwrap();
+        println!("Listening on {:?}:{}", self.addr, self.port);
+
+        loop {
+            match listener.accept() {
+                Ok((mut stream, _)) => {
+                    let mut buf = [0; 1024];
+                    match stream.read(&mut buf) {
+                        Ok(_) => println!("Received a request: {}", String::from_utf8_lossy(&buf)),
+                        Err(e) => println!("Failed to read from connection {}.", e),
+                    }
+                }
+                Err(e) => {
+                    println!("{}", e);
+                }
+            }
+        }
     }
 }
