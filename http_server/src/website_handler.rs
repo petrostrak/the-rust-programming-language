ebafdf997ae60cbe7f9b3ea80a7d4386349524bd
petrostrak <pit.trak@gmail.com>
2023-03-14 13:21:28 +0200

    Serve arbitrary files securely

--- http_server/src/website_handler.rs
+++ http_server/src/website_handler.rs
@@ -13,9 +13,19 @@ impl WebsiteHandler {
         Self { public_path }
     }
 
-    fn read_files(&self, file_path: &str) -> Option<String> {
+    fn read_file(&self, file_path: &str) -> Option<String> {
         let path = format!("{}/{}", self.public_path, file_path);
-        fs::read_to_string(path).ok()
+        match fs::canonicalize(path) {
+            Ok(path) => {
+                if path.starts_with(&self.public_path) {
+                    fs::read_to_string(path).ok()
+                } else {
+                    println!("Directory Traversal Attack Attempted: {}", file_path);
+                    None
+                }
+            }
+            Err(_) => None,
+        }
     }
 }
 
@@ -23,9 +33,12 @@ impl Handler for WebsiteHandler {
     fn handle_request(&mut self, req: &crate::request::Request) -> Response {
         match req.method() {
             &Method::GET => match req.path() {
-                "/" => Response::new(StatusCode::OK, self.read_files("index.html")),
-                "/hello" => Response::new(StatusCode::OK, self.read_files("hello.html")),
-                _ => Response::new(StatusCode::NOT_FOUND, None),
+                "/" => Response::new(StatusCode::OK, self.read_file("index.html")),
+                "/hello" => Response::new(StatusCode::OK, self.read_file("hello.html")),
+                path => match self.read_file(path) {
+                    Some(content) => Response::new(StatusCode::OK, Some(content)),
+                    None => Response::new(StatusCode::NOT_FOUND, None),
+                },
             },
             _ => Response::new(StatusCode::NOT_FOUND, None),
         }
