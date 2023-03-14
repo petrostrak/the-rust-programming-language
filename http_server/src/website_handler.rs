ec68ed11d9e47a215f94e12809d78c887e472bd9
petrostrak <pit.trak@gmail.com>
2023-03-14 13:06:43 +0200

    Serve html files

--- http_server/src/website_handler.rs
+++ http_server/src/website_handler.rs
@@ -1,3 +1,5 @@
+use std::fs;
+
 use http::{Method, StatusCode};
 
 use crate::{response::Response, server::Handler};
@@ -10,14 +12,19 @@ impl WebsiteHandler {
     pub fn new(public_path: String) -> Self {
         Self { public_path }
     }
+
+    fn read_files(&self, file_path: &str) -> Option<String> {
+        let path = format!("{}/{}", self.public_path, file_path);
+        fs::read_to_string(path).ok()
+    }
 }
 
 impl Handler for WebsiteHandler {
     fn handle_request(&mut self, req: &crate::request::Request) -> Response {
         match req.method() {
             &Method::GET => match req.path() {
-                "/" => Response::new(StatusCode::OK, Some("<h1>Welcome!</h1>".to_string())),
-                "/hello" => Response::new(StatusCode::OK, Some("<h1>Hello Rust!</h1>".to_string())),
+                "/" => Response::new(StatusCode::OK, self.read_files("index.html")),
+                "/hello" => Response::new(StatusCode::OK, self.read_files("hello.html")),
                 _ => Response::new(StatusCode::NOT_FOUND, None),
             },
             _ => Response::new(StatusCode::NOT_FOUND, None),
