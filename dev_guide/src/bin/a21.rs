a6c248eb4fe55f6fbce523d17cff8ee7af99f076
petrostrak <pit.trak@gmail.com>
2023-03-16 16:36:38 +0200

    Map on Option

---
+++ dev_guide/src/bin/a21.rs
@@ -0,0 +1,39 @@
+// Topic: Map combinator
+//
+// Requirements:
+// * Given a user name, create and print out a User struct if the user exists
+//
+// Notes:
+// * Use the existing find_user function to locate a user
+// * Use the map function to create the User
+// * Print out the User struct if found, or a "not found" message if not
+
+#[derive(Debug)]
+struct User {
+    user_id: i32,
+    name: String,
+}
+
+/// Locates a user id based on the name.
+fn find_user(name: &str) -> Option<i32> {
+    let name = name.to_lowercase();
+    match name.as_str() {
+        "sam" => Some(1),
+        "matt" => Some(5),
+        "katie" => Some(9),
+        _ => None,
+    }
+}
+
+fn main() {
+    let user_name = "matt";
+    let sam = find_user(user_name).map(|id| User {
+        name: user_name.to_owned(),
+        user_id: id,
+    });
+
+    match sam {
+        Some(user) => println!("{:?}", user),
+        None => println!("Not found."),
+    }
+}
