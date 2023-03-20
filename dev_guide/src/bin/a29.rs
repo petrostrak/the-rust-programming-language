f381596b087ad3620419ce6b14deddc306df8d23
petrostrak <pit.trak@gmail.com>
2023-03-20 13:01:41 +0200

    generic functions

---
+++ dev_guide/src/bin/a29.rs
@@ -0,0 +1,52 @@
+// Topic: Generics & Functions
+//
+// Requirements:
+// * Create a function that accepts the Priority trait as a generic parameter
+//   * The function should print out the guest and their priority
+// * Use the function with at least two different guests
+//
+// Notes:
+// * Use the debug token "{:?}" to print out the information
+// * Use the compiler to guide you to the correct generic constraints needed
+
+use std::fmt::Debug;
+
+#[derive(Debug)]
+enum ServicePriority {
+    High,
+    Standard,
+}
+
+trait Priority {
+    fn get_priority(&self) -> ServicePriority;
+}
+
+#[derive(Debug)]
+struct ImportantGuest;
+impl Priority for ImportantGuest {
+    fn get_priority(&self) -> ServicePriority {
+        ServicePriority::High
+    }
+}
+
+#[derive(Debug)]
+struct Guest;
+impl Priority for Guest {
+    fn get_priority(&self) -> ServicePriority {
+        ServicePriority::Standard
+    }
+}
+
+fn access<T>(guest: T)
+where
+    T: Priority + std::fmt::Debug,
+{
+    println!("{:?} is {:?} priority", guest, guest.get_priority());
+}
+
+fn main() {
+    let common: Guest = Guest;
+    let vip: ImportantGuest = ImportantGuest;
+    access(common);
+    access(vip);
+}
