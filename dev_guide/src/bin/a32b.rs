ed8ec0fd70832899cf5cd6eb8783c6285a4e57db
petrostrak <pit.trak@gmail.com>
2023-03-21 18:07:24 +0200

    lifetimes

---
+++ dev_guide/src/bin/a32b.rs
@@ -0,0 +1,80 @@
+#[derive(Debug)]
+struct Cards {
+    inner: Vec<IdCard>,
+}
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+enum City {
+    Barland,
+    Bazopolis,
+    Fooville,
+}
+
+#[derive(Debug)]
+struct IdCard {
+    name: String,
+    age: u8,
+    city: City,
+}
+
+impl IdCard {
+    pub fn new(name: &str, age: u8, city: City) -> Self {
+        Self {
+            name: name.to_string(),
+            age,
+            city,
+        }
+    }
+}
+
+fn new_ids() -> Cards {
+    Cards {
+        inner: vec![
+            IdCard::new("Amy", 1, City::Fooville),
+            IdCard::new("Matt", 10, City::Barland),
+            IdCard::new("Bailee", 20, City::Barland),
+            IdCard::new("Anthony", 30, City::Bazopolis),
+            IdCard::new("Tine", 40, City::Bazopolis),
+        ],
+    }
+}
+
+#[derive(Debug)]
+struct YoungPeople<'a> {
+    inner: Vec<&'a IdCard>,
+}
+
+impl<'a> YoungPeople<'a> {
+    fn living_in_fooville(&self) -> Self {
+        Self {
+            inner: self
+                .inner
+                .iter()
+                .filter(|id| id.city == City::Fooville)
+                .map(|id| *id)
+                .collect(),
+        }
+    }
+}
+
+fn main() {
+    let ids = new_ids();
+    let young = YoungPeople {
+        inner: ids.inner.iter().filter(|id| id.age <= 20).collect(),
+    };
+
+    println!("All ids:");
+    for id in ids.inner.iter() {
+        println!("{:?}", id)
+    }
+
+    println!("\nyoung ids:");
+    for id in young.inner.iter() {
+        println!("{:?}", id)
+    }
+
+    println!("\nliving in Fooville:");
+    for id in young.living_in_fooville().inner.iter() {
+        println!("{:?}", id)
+    }
+}
