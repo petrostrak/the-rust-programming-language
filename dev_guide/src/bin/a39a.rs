8738993d3ebdcfddbc395566a9137512b6921491
petrostrak <pit.trak@gmail.com>
2023-03-25 19:16:32 +0200

    Channels

---
+++ dev_guide/src/bin/a39a.rs
@@ -0,0 +1,36 @@
+// Channels
+use crossbeam_channel::unbounded;
+use std::thread;
+
+enum ThreadMsg {
+    PrintData(String),
+    Sum(i64, i64),
+    Quit,
+}
+
+fn main() {
+    let (s, r) = unbounded::<ThreadMsg>();
+    let handle = thread::spawn(move || loop {
+        match r.recv() {
+            Ok(msg) => match msg {
+                ThreadMsg::PrintData(d) => println!("{}", d),
+                ThreadMsg::Sum(lhs, rhs) => println!("{}+{}={}", lhs, rhs, lhs + rhs),
+                ThreadMsg::Quit => {
+                    println!("thread terminating");
+                    break;
+                }
+            },
+            Err(e) => {
+                println!("disconnected");
+                break;
+            }
+        }
+    });
+
+    s.send(ThreadMsg::PrintData("hello from main".to_owned()))
+        .unwrap();
+    s.send(ThreadMsg::Sum(10, 10)).unwrap();
+    s.send(ThreadMsg::Quit).unwrap();
+    // drop(s);
+    handle.join().unwrap();
+}
